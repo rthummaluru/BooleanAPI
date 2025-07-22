30489bd7685b2ff9378594aa240885154a6b3cbc
rthummaluru <120275888+rthummaluru@users.noreply.github.com>
2025-07-22 12:07:58 -0400

    Added in CORS middleware to allow requests from Frontend

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import router
 
 app = FastAPI(
@@ -8,3 +9,10 @@ app = FastAPI(
 )
 
 app.include_router(router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
